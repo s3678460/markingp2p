812d0f184b29145706f046db5b56527d664ea921
Lee Khanh <lkgame1993@gmail.com>
2020-03-28 01:04:10 +1100

    admin page

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,27 +1,67 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
 import './Admin.css'
 import logo from '../../library/admin-logo.png'
 class Admin extends Component {
+    constructor() {
+        super();
+        this.state = {
+            userID: '',
+            userPassword: ''
+        }
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        const { history } = this.props;
+        const userData = {
+            userID: this.state.userID,
+            userPassword: this.state.userPassword
+        }
+
+        if (userData.userID === "admin" && userData.userPassword === "admin") {
+            window.alert("Login successful!!!")
+            history.push('/admin')
+        } else {
+            window.alert("Username or password is wrong!!!!")
+        }
+    }
     render() {
         return (
             <div className="wrapper">
                 <section className="admin-form-section">
-                <form className="text-center  p-5" action="#!">
-                    <p>
-                        <img src={logo} width={50} height={50} alt="admin"/>
-                    </p>
-                    <p className="h4 mb-4">Sign in</p>
-                    {/* Email */}
-                    <input type="email" id="defaultLoginFormEmail" className="form-control mb-4" placeholder="E-mail" />
-                    {/* Password */}
-                    <input type="password" id="defaultLoginFormPassword" className="form-control mb-4" placeholder="Password" />
-                    
-                    {/* Sign in button */}
-                    <Link to="/admin"><button className="btn btn-info btn-block my-4" type="submit">Sign in</button></Link>
-                    {/* Register */}
-                    
-                </form>
+                    <form className="text-center p-5" onSubmit={this.onSubmit}>
+                        <p>
+                            <img src={logo} width={50} height={50} alt="admin" />
+                        </p>
+                        <p className="h4 mb-4">Sign in</p>
+                        {/* User ID */}
+                        <input
+                            onChange={this.onChange}
+                            type="text"
+                            name="userID"
+                            value={this.state.userID}
+                            id="defaultLoginFormEmail"
+                            className="form-control mb-4"
+                            placeholder="User ID" />
+                        {/* Password */}
+                        <input
+                            onChange={this.onChange}
+                            type="password"
+                            name="userPassword"
+                            value={this.state.userPassword}
+                            id="defaultLoginFormPassword"
+                            className="form-control mb-4"
+                            placeholder="Password" />
+
+                        {/* Sign in button */}
+                        <button className="btn btn-info btn-block my-4" type="submit">Login</button>
+                        {/* Register */}
+                    </form>
                 </section>
             </div>
         )
--- client/src/components/AdminPage/AdminPage.js
+++ client/src/components/AdminPage/AdminPage.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react'
 
 export default class AdminPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+}
     render() {
         return (
             <div>
-                List students marking
+                <div class="container">
+                    <div className="pt-5"><button className="btn btn-success">Create a Group</button></div>
+                    <div className="py-5">
+                        {/* per group */}
+                        <h1>Group1</h1>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Student Name</th>
+                                    <th scope="col">Student Number</th>
+                                    <th scope="col">Score</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Vo Tran Nhut Khanh</td>
+                                    <td>s3694787</td>
+                                    <td>10</td>
+                                </tr>
+                                <tr>
+                                    <td>Huynh Cong Minh</td>
+                                    <td>s3678460</td>
+                                    <td>10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button className="btn btn-danger">Delete</button>
+                    </div>
+                    <div className="py-5">
+                        {/* per group */}
+                        <h1>Group2</h1>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Student Name</th>
+                                    <th scope="col">Student Number</th>
+                                    <th scope="col">Score</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Yi Fang</td>
+                                    <td>s1234567</td>
+                                    <td>8</td>
+                                </tr>
+                                <tr>
+                                    <td>CoCo</td>
+                                    <td>s7654321</td>
+                                    <td>6</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button className="btn btn-danger">Delete</button>
+                    </div>
+                </div>
             </div>
         )
     }
